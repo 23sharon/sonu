6b15b2b49eb869bae5c56141b094657b990f162e
23sharon <36360017+23sharon@users.noreply.github.com>
2019-10-01 04:06:39 -0700

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,21 @@
+class sample
+{
+	int a;
+	sample()
+	{
+	a=10;
+	
+	}
+	Sample()
+	{
+	
+	void pr()
+	{
+	System.out.println(a);
+	}
+	public static void main(String args[])
+	{
+		sample ss=new sample();
+		ss.pr();
+	}
+}
